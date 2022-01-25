1aca3c465094c8dd3390f1829fd3abb17bd7c7f2
JuvitaSaini09 <juvitasaini@gmail.com>
2022-01-25 08:43:40 +0530

    docs: document updated

--- Tab component/tab.js
+++ Tab component/tab.js
@@ -1,12 +1,32 @@
 const tabsBtn = document.querySelectorAll(".tabs");
 const tabContent = document.querySelectorAll(".tab-content");
-tabContent[0].style.display = "none";
+
+tabContent[0].style.display = "block";
+for(var k=1;k<tabContent.length;k++){
+    tabContent[k].style.display = "none";   
+}
+/*
 tabContent[1].style.display = "none";
 tabContent[2].style.display = "none";
+*/
 
-tabsBtn.forEach((eachList) =>{
+tabsBtn.forEach((eachList,i) =>{
     eachList.addEventListener("click", () =>{
-        console.log("hi")
+        let a=0;
+        tabContent[i].style.display = "block";
+        tabsBtn[i].style.color="purple";
+        tabsBtn[i].style.borderBottom="2px purple solid";
+
+        for(let a=0;a<tabsBtn.length;a++)
+        {
+        if(a!=i)
+        {tabContent[a].style.display = "none";
+        tabsBtn[a].style.color="rgb(51, 49, 49)";
+        tabsBtn[a].style.border="1px solid white"
+        }
+        }
+        a++;
+        
     })
 })
 
